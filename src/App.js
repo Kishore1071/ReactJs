3e26aa0f6cf4223c757eb662a1ef3753a3a63959
Kishore1071 <kishore240398@gmail.com>
2024-01-28 21:41:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import SingleInputFields from './concepts/dynamic-input-fields/SingleInputFields';
-import InputWithMultipleFields from './concepts/dynamic-input-fields/InputWithMultipleFields';
 
 function App() {
   return (
     <div>
-        <SingleInputFields></SingleInputFields>
-        <InputWithMultipleFields></InputWithMultipleFields>
+
     </div>
   );
 }
